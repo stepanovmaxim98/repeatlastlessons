65c5a1fc4b8c262d30cf322c9bf33a4d0b549ed1
Maxim <stepanovmaxim98@yandex.ru>
2022-11-22 20:35:11 +0300

    new learns of function

--- js/script.js
+++ js/script.js
@@ -1,44 +1,12 @@
-"use strict";
+"use strict"
 
-const objFirst = {
-    count: 500,
-    first: 300,
-    manyObj: {
-        obj:1,
-        obj:2,
-        obj:3
-    },
-    manyOtherOption: true
-};
-
-const firstArr = [];
-firstArr[0] = prompt('Ответ на впрос');
-firstArr[1] = alert('Верни ответ');
-firstArr[2] = confirm('Да или нет');
-
-const userName = prompt('Введите свое имя');
-const answerName = alert(`Привет ${userName}`);
-
-// logical operation
-
-const meatProduct = true;
-const brokkolliProduct  = true;
-
-console.log(meatProduct && brokkolliProduct);
-console.log(meatProduct || brokkolliProduct);
-console.log(!meatProduct && !brokkolliProduct);
-
-let a = 10;
-    b = 13;
-
-if (a > 5){
-    console.log('More, than');
-} else{
-    console.log('No more');
+function sayHello(name) {
+    return `Привет, ${name}!`;
 }
 
-switch (a){
-    case 49:
-        console.log('Привеь!');
-        break;
+sayHello('Alex');
+
+function returnNeighboringNumbers(num) {
+    return [num - 1, num, num + 1];
 }
+returnNeighboringNumbers(4);
